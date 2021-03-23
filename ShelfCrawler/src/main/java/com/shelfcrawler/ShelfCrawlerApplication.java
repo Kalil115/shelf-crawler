4b7f2e4b3bb6c17d13de91020ecd9f165895671d
YYC <YYC@DESKTOP-RUT4CTP>
2021-03-23 14:30:21 -0500

    project init

---
+++ ShelfCrawler/src/main/java/com/shelfcrawler/ShelfCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.shelfcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfCrawlerApplication.class, args);
+	}
+
+}
---
+++ ShelfCrawler/src/test/java/com/shelfcrawler/ShelfCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelfcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
