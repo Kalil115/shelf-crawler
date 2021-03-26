e7933cfe107119ca3715155bfa9bf238ba45ad1c
YYC <YYC@DESKTOP-RUT4CTP>
2021-03-26 18:11:52 -0500

    added angular project

---
+++ ShelfCrawler/src/main/java/com/shelfcrawler/controller/BookShelfController.java
@@ -0,0 +1,12 @@
+package com.shelfcrawler.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bookshelf")
+public class BookShelfController {
+
+	
+	
+}
