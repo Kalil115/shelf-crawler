5b17ef9768330db8bf1cf975579c5e2591ab4b72
YYC <YYC@DESKTOP-RUT4CTP>
2021-03-31 19:43:08 -0500

    added Jasypt to encode properties

--- ShelfCrawler/src/main/java/com/shelfcrawler/ShelfCrawlerApplication.java
+++ ShelfCrawler/src/main/java/com/shelfcrawler/ShelfCrawlerApplication.java
@@ -2,11 +2,13 @@ package com.shelfcrawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@PropertySource("encryptor.properties")
 public class ShelfCrawlerApplication {
 
 	public static void main(String[] args) {
